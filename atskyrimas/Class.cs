0a62d109e1685f6df2e100914ea7f70040b7f022
Pauksciukas <Pauksciukas@RUTA>
2022-03-05 19:08:55 +0200

    Add project files.

---
+++ atskyrimas/Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bandymas
+{
+    internal class matematika
+    {
+        public int n = 2;
+        public long ats = 0;
+        public void progresija()
+        {
+            long i = 1;
+            long b = 2;
+            double end = Math.Pow(10, 15);
+            while (ats <= end)
+            {
+                ats = i * b;
+                i = b;
+                b = ats;
+                n++;
+            }
+        }
+    }
+}
---
+++ atskyrimas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace bandymas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Thread.Sleep(1000);
+            stopwatch.Stop();
+
+            TimeSpan ts = stopwatch.Elapsed;
+
+            Console.WriteLine("Elapsed Time is {0:00}:{1:00}:{2:00}.{3}",
+                            ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+            Console.WriteLine("-----------------------");
+
+            //kuriamas objektas
+            matematika obj = new matematika();
+            obj.progresija();
+
+            Console.WriteLine("Reikia elementu: " + obj.n);
+            Console.WriteLine("Galutinis atsakymas: " + obj.ats);
+
+            Console.ReadLine();
+        }
+    }
+}
