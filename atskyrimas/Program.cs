5caf28762471b304d3fa41943c2164c24530e552
Rūta P <ruta.plac@gmail.com>
2022-03-07 16:27:30 +0200

    ..

--- atskyrimas/Program.cs
+++ atskyrimas/Program.cs
@@ -29,6 +29,7 @@ namespace bandymas
             objk.mate();
 
             Console.WriteLine("--SEKOS SKAICIAVIMAS--");
+            Console.WriteLine("----------");
             Console.WriteLine("Reikia elementu: " + obj.n);
             Console.WriteLine("Galutinis atsakymas: " + obj.ats);
             Console.WriteLine();
